31243693027efce112f8129073efb00076200c28
joker10130 <narongrit.rodphroboon@outlook.co.th>
2020-02-16 15:36:20 +0700

    Prepared for deployment

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use('/api/profile', require('./routes/api/profile'));
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
